e2c5daea192758de08c51655f4f3cc1a7cecb9a2
munpie <munpie07@yahoo.com>
2018-05-07 17:18:27 -0700

    Update data_movement.js

--- js/data_movement.js
+++ js/data_movement.js
@@ -128,7 +128,8 @@ data_movement = [
         description: "A creature can try to tumble through a hostile creature’s space, ducking and weaving past the opponent.",
         reference: "DMG, pg. 271-272.",
         bullets: [
-            "As an action or a bonus action, the tumbler makes a Dexterity (Acrobatics) check contested by the hostile creature’s Dexterity (Acrobatics) check. If the tumbler wins the contest, it can move through the hostile creature’s space once this turn."
+            "As an action or a bonus action, the tumbler makes a Dexterity (Acrobatics) check contested by the hostile creature’s Dexterity (Acrobatics) check."
+            "If the tumbler wins the contest, it can move through the hostile creature’s space once this turn."
         ]
     }
 ]
